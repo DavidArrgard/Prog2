a488205ad97cdc74ec8c4f17c0541f230a62a04a
DavidA <david.arrgard@skola.taby.se>
2021-09-02 08:56:29 +0200

    Uppgift 1 lektion 1.2

---
+++ delbar uppgift/uppgift.py
@@ -0,0 +1,3 @@
+for i in range(1000):
+    if i % 7 == 0 or 7 in divmod(i, 10):
+        print(i)
---
+++ rövarspråk.py
@@ -0,0 +1,14 @@
+x = input("Ohoooy, Vad vill du skriva på rövarspråket? ")
+k = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n",
+     "p", "q", "r", "s", "t", "v", "w", "z", "x"]
+ord = ""
+
+for y in x:
+    if x in k:
+        ord = ord.x
+    else:
+        ord = ord+x+"o"+x
+print(ord)
+
+
+# koden funkar inte holger :D
