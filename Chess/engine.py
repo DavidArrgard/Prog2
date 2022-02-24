0062cb26f01d95b3a2faa1a7be9813cf912a1769
DavidA <david.arrgard@skola.taby.se>
2022-02-24 10:57:29 +0100

    Move Pieces

--- Chess/engine.py
+++ Chess/engine.py
@@ -1,3 +1,7 @@
+
+from shutil import move
+
+
 class Game():
     def __init__(self):
         self.board =[
@@ -9,4 +13,187 @@ class Game():
             ["--","--","--","--","--","--","--","--"],
             ["wp","wp","wp","wp","wp","wp","wp","wp"],
             ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
+        self.moveFunc = {'p': self.getPmoves, 'R': self.getRmoves, 'N': self.getNmoves, 'B': self.getBmoves,
+                         'Q': self.getQmoves, 'K': self.getKmoves}
         self.whiteToMove = True
+        self.moveList = []
+        self.whiteKPos = (7, 4)
+        self.blackKPos = (0, 4)
+
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = '--'
+        self.board[move.endRow][move.endCol] = move.movePiece
+        self.moveList.append(move)
+        self.whiteToMove = not self.whiteToMove
+        if move.movePiece == 'wK':
+            self.whiteKPos = (move.endRow, move.endCol)
+        if move.movePiece == 'bK':
+            self.whiteKPos = (move.endRow, move.endCol)
+
+    def undoMove(self):
+        if len(self.moveList) != 0:
+            move = self.moveList.pop()
+            self.board[move.startRow][move.startCol] = move.movePiece
+            self.board[move.endRow][move.endCol] = move.capuredPiece
+            self.whiteToMove = not self.whiteToMove
+            if move.movePiece == 'wK':
+                self.whiteKPos = (move.endRow, move.endCol)
+            if move.movePiece == 'bK':
+                self.whiteKPos = (move.endRow, move.endCol)
+
+    def vMoves(self):
+        moves = self.possibleMoves()
+        for i in range(len(moves)-1, -1, -1):
+            self.makeMove(moves[i])
+            self.whiteToMove = not self.whiteToMove
+            if self.inC():
+                moves.remove[moves[i]]
+            self.whiteToMove = not self.whiteToMove
+            self.undoMove()
+        return moves
+
+    def inC(self):
+        if self.whiteToMove:
+            return self.underAttack(self.whiteKPos[0], self.whiteKPos[1])
+        else:
+            return self.underAttack(self.blackKPos[0], self.blackKPos[1])
+
+    def underAttack(self, r, c):
+        self.whiteToMove = not self.whiteToMove
+        oppMove = self.vMoves()
+        self.whiteToMove = not self.whiteToMove
+        for move in oppMove:
+            if move.endRow == r and move.endCol == c:
+                self.whiteToMove = not self.whiteToMove
+                return True
+        return False
+
+    def possibleMoves(self):
+        m = []
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
+                    p = self.board[r][c][1]
+                    self.moveFunc[p](r, c, m)
+        return m
+    
+    def getPmoves(self, r, c, m):
+        if self.whiteToMove:
+            if self.board[r-1][c] == "--":
+                m.append(Move((r,c), (r-1,c), self.board))
+                if r == 6 and self.board[r-2][c] == "--":
+                    m.append(Move((r, c), (r-2, c), self.board))
+            if c-1 >= 0:
+                if self.board[r-1][c-1][0] == 'b':
+                    m.append(Move((r, c), (r-1, c-1), self.board))
+            if c+1 <= 7:
+                if self.board[r-1][c+1][0] == 'b':
+                    m.append(Move((r, c), (r-1, c+1), self.board))
+        else:
+            if self.board[r + 1][c] == "--":
+                m.append(Move((r, c),(r + 1, c), self.board))
+                if r == 1 and self.board[r + 2][c] == "--":
+                    m.append(Move((r, c), (r + 2, c), self.board))
+
+            if c - 1 >= 0:
+                if self.board[r + 1][c - 1][0] == "w":
+                    m.append(Move((r, c), (r + 1, c - 1), self.board))
+            if c + 1 <= 7:
+                if self.board[r + 1][c + 1][0] == "w":
+                    m.append(Move((r, c), (r + 1, c + 1), self.board))
+
+    def getRmoves(self, r, c, m):
+        dirrect = ((-1,0), (0, -1), (1, 0), (0, 1))
+        eColor = "b" if self.whiteToMove else "w"
+        for d in dirrect:
+            for i in range(1,8):
+                endRow = r + d[0] * i
+                endCol = c + d[1] * i
+                if 0 <= endRow < 8  and 0 <= endCol < 8:
+                    endP = self.board[endRow][endCol]
+                    if endP == "--":
+                        m.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endP[0] == eColor:
+                        m.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
+
+    def getNmoves(self, r, c, m):
+        kM = ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1))
+        aColor = "w" if self.whiteToMove else "b"
+        for i in kM:
+            endRow = r + i[0]
+            endCol = c + i[1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endP = self.board[endRow][endCol]
+                if endP[0] != aColor:
+                    m.append(Move((r, c), (endRow, endCol), self.board))
+
+    def getBmoves(self, r, c, m):
+        dirrect = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        eColor = "b" if self.whiteToMove else "b"
+        for d in dirrect:
+            for i in range(1, 8):
+                endRow = r + d[0] * i
+                endCol = c + d[1] * i
+                if 0 <= endRow < 8 and 0 <= endCol < 8:
+                    endP = self.board[endRow][endCol]
+                    if endP == "--":
+                        m.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endP[0] == eColor:
+                        m.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
+
+    def getQmoves(self, r, c, m):
+        self.getRmoves(r, c, m)
+        self.getBmoves(r, c, m)
+
+    def getKmoves(self, r, c, m):
+        kM = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        aColor = "w" if self.whiteToMove else "b"
+        for i in range(8):
+            endRow = r + kM[i][0]
+            endCol = c + kM[i][1]
+            if 0 <= endRow < 8 and 0 <= endRow < 8:
+                endP = self.board[endRow][endCol]
+                if endP[0] != aColor:
+                    m.append(Move((r, c), (endRow, endCol), self.board))
+    
+class Move():
+
+    rowRanks = {1: '7', 2 :'6', 3: '5', 4: '4', 5: '3', 6: '2', 7: '1', 0: '8'}
+
+    rankRows = {v: k for k, v in rowRanks.items()}
+    filesCols = {'a': 0,'b': 1,'c': 2,'d': 3,'e': 4,'f': 5,'g': 6,'h': 7}
+
+    colsFiles = {v: k for k, v in filesCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.movePiece = board[self.startRow][self.startCol]
+        self.capuredPiece = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
+
+    def getNotation(self):
+        return self.getFile(self.startRow, self.startCol) + self.getFile(self.endRow, self.endCol)
+
+    def getFile(self, r, c):
+        return self.colsFiles[c] + self.rowRanks[int(r)]
+        
--- Chess/game.py
+++ Chess/game.py
@@ -1,9 +1,9 @@
 from email.mime import image
 import pygame as p
-from engine import Game
+from engine import Game, Move
 
 
-width = height = 512 
+width = height = 512
 dimensions = 8
 SQ_size = height/8
 MAX_FPS = 15
@@ -20,13 +20,47 @@ def main():
     screen = p.display.set_mode((width,height))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
-    gs = Game()  
+    gs = Game()
+    vMoves = gs.vMoves()
+    moveMade = False
     loadImages()
+    sqSelected = ()
+    playerClick = []
     running = True
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                pos = p.mouse.get_pos()
+                col = pos[0]//SQ_size
+                row = pos[1]//SQ_size
+                if sqSelected == (row, col):
+                    sqSelected = []
+                    playerClick = []
+                else:
+                    sqSelected = (int(row), int(col))
+                    playerClick.append(sqSelected)
+                if len(playerClick) == 2:
+                    move = Move(playerClick[0],playerClick[1], gs.board)
+                    print(move.getNotation())
+                    if move in vMoves:
+                        gs.makeMove(move)
+                        moveMade = True
+                        sqSelected =  {}
+                        playerClick = []
+                    else:
+                        playerClick[sqSelected]
+
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undoMove()
+                    moveMade = True
+
+        if moveMade:
+            vMoves = gs.possibleMoves()
+            moveMade = False    
+
         GameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -52,8 +86,6 @@ def drawPieces(screen, board):
             if piece != "--":
                 screen.blit(images[piece], p.Rect(c*SQ_size,r*SQ_size, SQ_size, SQ_size))
 
-""""
+
 if __name__ == "__main__":
     main()
-"""
-main()
